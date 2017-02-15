8e1e91f2f8a8d3fe8d2c49748ea46de6c9e52dac
Lucas Lopilato <lopilato.lucas@gmail.com>
2017-02-14 22:42:41 -0800

    Test Cleanse and Cleanse negation

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from FileParser import *
 class TestFileParser(unittest.TestCase):
     @classmethod
     def setUpClass(self):
-        self.p = FileParser('testing.txt')
+        self.p = FileParser('input/testing.txt')
 
     @classmethod
     def tearDownClass(self):
@@ -16,8 +16,19 @@ class TestFileParser(unittest.TestCase):
         self.assertIn('the', self.p.blacklist)
         self.assertIn('and', self.p.blacklist)
 
-    def test_nontext(self):
+    def test_cleanse(self):
         self.assertEqual(self.p.cleanse("123512*&^^%@$!@^*&"), {})
+        self.assertEqual(self.p.cleanse("ron"), {'ron': 1})
+        self.assertEqual(self.p.cleanse("12 rom 45 molem 4()"),
+            {'rom': 1, 'molem': 1})
+
+    def test_cleanseNegation(self):
+        self.assertEqual(self.p.cleanse("not not not not not"), {})
+        self.assertEqual(self.p.cleanse("not not not not not me"), {'me': -1})
+        self.assertEqual(self.p.cleanse("not not not not me"), {'me': 1})
+        self.assertEqual(self.p.cleanse("not not tim not not not me"),
+            {'tim': 1, 'me': -1})
+
 
 
 if __name__ == '__main__':
