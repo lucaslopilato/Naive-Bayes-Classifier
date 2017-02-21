b812e25def83aa78a593f87331ecf922ccb95c61
Lucas Lopilato <lopilato.lucas@gmail.com>
2017-02-20 21:03:24 -0800

    Added Comments and Main

--- NaiveBayesClassifier.py
+++ NaiveBayesClassifier.py
@@ -2,11 +2,14 @@
 # CS165A Machine Problem 1
 # Lucas Lopilato
 
-# import sys
-from collections import defaultdict
-from FileParser import *
-import time
+# System Imports
+from sys import argv
 from math import log10
+from time import time
+from collections import defaultdict
+
+# Local Imports
+from FileParser import *
 
 
 class NaiveBayesClassifier(object):
@@ -14,18 +17,11 @@ class NaiveBayesClassifier(object):
     def __init__(self,
                  training="input/training.txt",
                  testing="input/testing.txt"):
-        # TODO Remove
-        # Parse Command Line Arguments
-        # args = str(sys.argv)
-        """ if(len(args) != 3):
-        print("proper usage: python \
-        NaiveBayesClassifier.py training.txt testing.txt")"""
-        # Begin Timing
 
         # Train
-        traint = time.time()
+        traint = time()
         self.train = FileParser(training)
-        traint = time.time() - traint
+        traint = time() - traint
 
         # Calculate Probability Of Each Class
         total = float(self.train.positiveDocs + self.train.negativeDocs)
@@ -33,24 +29,27 @@ class NaiveBayesClassifier(object):
         self.negative = self.train.negativeDocs / total
 
         # Test Against Testing Set
-        testt = time.time()
+        testt = time()
         trainAcc = self.test(testing, True)
-        testt = time.time() - testt
+        testt = time() - testt
 
+        # Print Results
+        print("%d seconds (training)" % traint)
+        print("%d seconds (testing)" % testt)
+
+        # Test and Print Results against training set
         print("%0.3f (training)" % self.test(training))
         print("%0.3f (testing)" % trainAcc)
-        print("%1f seconds (training)" % traint)
-        print("%1f seconds (testing)" % testt)
 
-    # Run a full test
+    # Run a file against the algorithm
     def test(self, file, pr=False):
         # Initialize Testing Variables
-        correct = 0.0
-        total = 0.0
-        expected = -1
-        actual = -1
+        correct = 0.0  # Number Correct
+        total = 0.0  # Number Total
+        expected = -1  # Expected Value for a test
+        actual = -1  # Actual Value for a test
 
-        # Test Training Against Itself
+        # Get Tests To Run
         with open(file, 'r') as tests:
             for review in tests:
                 # Count Total Tests
@@ -63,6 +62,7 @@ class NaiveBayesClassifier(object):
                 self.train.cleanse(review, words)  # Read In Review's Words
                 actual = self.guess(words)  # Guess Based on Words
 
+                # Print If Needed
                 if pr:
                     print(actual)
 
@@ -94,4 +94,8 @@ class NaiveBayesClassifier(object):
 
 
 if __name__ == '__main__':
-    init = NaiveBayesClassifier()
+    if len(argv) != 3:
+        print('Usage: python NaiveBayesClassifier.py training.txt testing.txt')
+        exit(1)
+
+    init = NaiveBayesClassifier(argv[1], argv[2])
